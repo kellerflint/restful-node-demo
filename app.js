f0c71c3473413f95ca3ed00fa02ed7a81da41d09
Keller Flint <kellerflint@Kellers-MacBook-Air.local>
2021-11-30 14:12:04 -0700

    create express app

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
